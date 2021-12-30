4f8cd824c03c6ba82d08544b43edb1c72ff05e8a
Dominick Baier <dbaier@leastprivilege.com>
2021-12-30 23:36:05 +0100

    add logging

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Duende.IdentityServer.Demo;
 using Serilog;
+using Serilog.Events;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
@@ -11,15 +12,34 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
-    builder.Host.UseSerilog((ctx, lc) => lc
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-        .Enrich.FromLogContext()
-        .ReadFrom.Configuration(ctx.Configuration));
+    builder.Host.UseSerilog((ctx, lc) =>
+    {
+        lc
+            .MinimumLevel.Debug()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+            .MinimumLevel.Override("System", LogEventLevel.Warning)
+            .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+            .WriteTo.Console(
+                outputTemplate:
+                "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+            .Enrich.FromLogContext();
+
+        // if (ctx.HostingEnvironment.IsProduction())
+        // {
+        //     lc.WriteTo.File(@"/home/LogFiles/Application/identityserver.txt",
+        //         fileSizeLimitBytes: 1_000_000,
+        //         rollOnFileSizeLimit: true,
+        //         shared: true,
+        //         flushToDiskInterval: TimeSpan.FromSeconds(1));
+        // }
+    });
+        
 
     var app = builder
         .ConfigureServices()
         .ConfigurePipeline();
-    
+
     app.Run();
 }
 catch (Exception ex)
