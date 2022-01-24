6dd2044a6c0a122b1af6a1137cef179d99455ac2
Dominick Baier <dbaier@leastprivilege.com>
2022-01-24 12:07:20 +0100

    Update Index.cshtml.cs

--- src/Pages/Diagnostics/Index.cshtml.cs
+++ src/Pages/Diagnostics/Index.cshtml.cs
@@ -15,12 +15,6 @@ public class Index : PageModel
         
     public async Task<IActionResult> OnGet()
     {
-        var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-        if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-        {
-            return NotFound();
-        }
-
         View = new ViewModel(await HttpContext.AuthenticateAsync());
             
         return Page();
