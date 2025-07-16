67bdb16b8286abbd34f2154629f6b2a6c0094254
Wesley Cabus <wesley@gotsharp.be>
2025-07-16 16:38:37 +0200

    Generate (and cache) a token to use as the default value

--- src/Pages/Home/JwtDecoder/JwtDecoder.cshtml.cs
+++ src/Pages/Home/JwtDecoder/JwtDecoder.cshtml.cs
@@ -1,9 +1,62 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using IdentityModel.Client;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace IdentityServerHost.Pages.Home;
 
 [AllowAnonymous]
-public class JwtDecoder : PageModel
+public class JwtDecoder(IHttpClientFactory clientFactory, IMemoryCache cache) : PageModel
 {
+    private const string CacheKey = "jwt_decoder::access_token";
+    
+    public ViewModel View { get; set; } = default!;
+
+    public async Task<IActionResult> OnGet()
+    {
+        var token = await GetOrCreateToken();
+        View = new ViewModel { Token = token };
+        
+        return Page();
+    }
+
+    private async Task<string> GetOrCreateToken()
+    {
+        try
+        {
+            if (cache.TryGetValue(CacheKey, out string cachedToken))
+            {
+                return cachedToken;
+            }
+            
+            var request = HttpContext.Request;
+            var authority = request.Scheme + "://" + request.Host.ToUriComponent();
+            
+            var client = clientFactory.CreateClient();
+            var response = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = $"{authority}/connect/token",
+
+                ClientId = "m2m",
+                ClientSecret = "secret",
+
+                Scope = "api"
+            });
+
+            if (!response.IsError)
+            {
+                var token = response.AccessToken;
+                cache.Set(CacheKey, token, TimeSpan.FromMinutes(20));
+                
+                return token;
+            }
+        }
+        catch
+        {
+            return null;
+        }
+        
+        return null;
+    }
 }
